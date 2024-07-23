bf2fc5694fe142d577f7adad3d9e2b6d1eafa591
Quentin Lasue <quentin.lasue.jm@free.fr>
2024-07-23 12:55:57 +0200

    enregistrement localstorage resultats

--- client/src/view/admin/AddResultCompetition.jsx
+++ client/src/view/admin/AddResultCompetition.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../appContext/AppContext";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -6,17 +6,29 @@ import DrawListResult from "../../component/DrawListResult";
 
 
 function AddResultCompetition(){
-    const {drawCompetition, setDrawCompetition }=useContext(AppContext)
+    const {drawCompetition, setDrawCompetition }=useContext(AppContext);
+    const [error, setError]= useState("");
+    const [success, setSuccess]= useState("");
 
     const initialResultsState = drawCompetition.reduce((acc, draw, index)=>{
-        acc[index]={};
+        const savedResults =JSON.parse(localStorage.getItem('results'))|| {};
+        acc[index]=savedResults[index] || {};
         draw.forEach((match, matchIndex) => {
-            acc[index][`match-${matchIndex + 1}`]= '';
+            if(!acc[index][`match-${matchIndex + 1}`]){
+                acc[index][`match-${matchIndex + 1}`]= '';
+            }
         });
         return acc;
     },{});
 
     const [results, setResults]= useState(initialResultsState);
+    const [finalResults, setFinalResults]= useState(initialResultsState); 
+    // sauvegarde dans le localstorage a chaque envoi des resultats
+    useEffect(() => {
+      localStorage.setItem('results', JSON.stringify(finalResults))
+
+    }, [finalResults]);
+    
 
     const handleChange = (drawIndex, matchIndex, value)=>{
         setResults({
@@ -27,18 +39,40 @@ function AddResultCompetition(){
             }}
         )
     }
+    const handleSubmit=(index)=>{
+        setError('');
+        setSuccess('');
+        // vérifier si tous les résultats sont entrée pour le tirage envoyer
+        let allFieldsFilled = true;
 
-    const handleSubmit = (event)=>{
+        for(let matchKey in results[index]){
+            if( results[index][matchKey] ===''){
+                allFieldsFilled = false;
+                break;
+            }
+        }
+
+        if(allFieldsFilled){
+            // enregistrer les résultats
+            setFinalResults(results);
+            //faire disparaitre le tirage qui a était envoyé.
+            setSuccess(`Les résultats du tirage n°${index+1} ont été envoyé.`)
+        }else{
+            setError(`Les résultats entrée ne sont pas complet pour le tirage n°${index+1}.`)
+        }
+        // Sinon message d'erreur ?
+    }
+    const handleSend = (event)=>{
         event.preventDefault();
-        // console.log(results[0]["match-1"][0].id_membre);
-        // console.log(results);
-        // console.log(results.length);
+        setError('');
+        setSuccess('');
         const listWinner = extractIdMembre(results);
         console.log(listWinner);
         if(Array.isArray(listWinner)){
-
+            console.log('resultat ok');
         }else{
             // gérer le cas ou listWinner est le msg d'erreur de resultats manquant 
+            setError(listWinner);
         }
     }
 
@@ -73,9 +107,15 @@ function AddResultCompetition(){
         <>
         { drawCompetition.length ? (
             <Container>
+                {error && <p style={{ color: 'red' }} className="mb-3">{error}</p>}
+                {success && <p style={{ color: 'green' }} className="mb-3">{success}</p>}
                 {drawCompetition.map((draw, index)=>(
                     <Row className="mb-3" key={index}>
-                        <Form onSubmit={handleSubmit}>
+                        <Form onSubmit={(event)=>{
+                                event.preventDefault();
+                                handleSubmit(index)}
+                                }
+                            >
                             <h3>Tirage n°{index+1}</h3>
                             <DrawListResult 
                             draw={draw} 
@@ -87,7 +127,7 @@ function AddResultCompetition(){
                         </Form>
                     </Row>
                 ))}
-                {/* <Button variant="warning">Finaliser l'envoi des résultats</Button> */}
+                <Button variant="warning" onClick={handleSend}>Finaliser l'enregistrement des résultats du concours</Button>
             </Container>
         ):(
             <Row>
